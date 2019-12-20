dbac391c38fc80a97dce3ff314f056e35199641e
Kimseonghun-468 <tjdgns4560@naver.com>
2019-12-20 19:02:34 +0900

    최종

--- 2019전디실과제.py
+++ 2019전디실과제.py
@@ -14,14 +14,46 @@ keywords = soup.find_all('dl', class_ ='movie_item')
 keywords = [line.get_text().strip() for line in keywords]
 
 key = []
+check = []
+Result1 = []
+Result2 = []
 for i in range(0,len(keywords)):
-    key += keywords[i].split()
-    
+    if keywords[i][0] in '개':
+        key += keywords[i].split()
+    else:
+        check.append(i)
 
-df = pd.DataFrame({ '제목' : Name[0:21],
-                   '개봉일' : key[1:127:6],
-                   '일간' : key[3:127:6],
-                   '누적' : key[5:127:6],
-                  '순위' : list(range(1,22))})
- 
-df
+j =0
+for i in check:
+    del(Name[i+j])
+    j-=1
+    
+for i in range(0,len(Name)):
+    Result1.append(int(key[(6*i)+3].replace(',', '').strip('명')))
+for i in range(0,len(Name)):
+    Result2.append(int(key[(6*i)+5].replace(',', '').strip('명')))
+
+df = pd.DataFrame({ '제목' : Name,
+                   '개봉일' : key[1::6],
+                   '일간' : Result1,
+                   '누적' : Result2,
+                  '랭킹' : list(range(1,len(Name)+1))})
+
+
+print(df.groupby('개봉일')['일간'].nunique())
+
+fig = plt.figure()
+axes1 = fig.add_subplot(1,2,1)
+axes2 = fig.add_subplot(1,2,2)
+axes1.hist(df['일간'],bins = 20)
+axes2.scatter(df['누적'],df['일간'])
+
+axes1.set_title("Day Spectators")
+axes1.set_xlabel("Spectators")
+axes1.set_ylabel('Frequency')
+axes2.set_title("Day Spectators / Accumulated Spectators")
+axes2.set_xlabel("Day Spectators")
+axes2.set_ylabel('Accumulated Spectators')
+
+fig.tight_layout()
+
